edd4dfb5ca77bb4a526e52138266886b1a2bf27f
ranxianrui <601539285@qq.com>
2022-09-22 22:33:41 +0800

    新增三张首页图

--- ruoyi-ui/src/views/cms/backgroundImages.js
+++ ruoyi-ui/src/views/cms/backgroundImages.js
@@ -3,10 +3,17 @@ import bg1 from '../../assets/images/background.jpg'
 import bg3 from '../../assets/images/aqua1.png'
 import bg4 from '../../assets/images/minatoAqua.png'
 import bg5 from '../../assets/images/starEnd.png'
+import bg6 from '../../assets/images/aquakin.jpg'
+import bg7 from '../../assets/images/startend_new.jpg'
+import bg8 from '../../assets/images/aquashion.jpg'
+
 
 export default[
 	bg1,
 	bg3,
 	bg4,
-	bg5
+	bg5,
+	bg6,
+	bg7,
+	bg8
 ]
